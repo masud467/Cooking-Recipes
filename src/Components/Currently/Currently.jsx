060a3e292c0274e2969a859f503cb0a28ac471a6
masud4657 <masudpfrnt@gmail.com>
2024-03-18 11:38:37 +0600

    added the final project

--- src/Components/Currently/Currently.jsx
+++ src/Components/Currently/Currently.jsx
@@ -1,7 +1,7 @@
 
 
-const Currently = (currentCook) => {
-    const {recipe_name,preparing_time,calories} = currentCook;
+const Currently = () => {
+    // const {recipe_name,preparing_time,calories} = currentCook;
     return (
         <div>
              <div className='flex gap-14 text-xl font-extrabold'>
@@ -10,9 +10,9 @@ const Currently = (currentCook) => {
                 <h2>Calories</h2>
             </div>
             <div className='flex gap-8 text-lg font-semibold bg-slate-200 p-4 rounded-2xl'>
-               <h2>{recipe_name}</h2>
-               <p>{preparing_time}</p>
-               <p>{calories} </p>
+               <h2>Chicken Stir-Fry</h2>
+               <p>30 minutes</p>
+               <p>350 calories</p>
               
             </div> 
         </div>
